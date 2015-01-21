4d507d076f60cbe62ffa4215fe92459cd2505274
yaobanglin <yaobanglin@163.com>
2015-01-21 09:16:28 +0800

    add LoginItemInfo  and edit layout

---
+++ abhegios/Class/Entity/User/LoginItemInfo.h
@@ -0,0 +1,15 @@
+//
+//  LoginItemInfo.h
+//  abhegios
+//
+//  Created by 180 on 15/1/21.
+//  Copyright (c) 2015年 180. All rights reserved.
+//
+
+#import "BaseEntity.h"
+#import "ILogin.h"
+@interface LoginItemInfo : BaseEntity
+@property LoginType type;
+@property(nonatomic, copy) NSString *title;
+@property(nonatomic, copy) NSString *icon;
+@end
---
+++ abhegios/Class/Entity/User/LoginItemInfo.m
@@ -0,0 +1,13 @@
+//
+//  LoginItemInfo.m
+//  abhegios
+//
+//  Created by 180 on 15/1/21.
+//  Copyright (c) 2015年 180. All rights reserved.
+//
+
+#import "LoginItemInfo.h"
+
+@implementation LoginItemInfo
+
+@end
