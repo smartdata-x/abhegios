43ad2b582864302da1a1fa175d6fa1244fc9f1c9
archer <archer@miglab.com>
2015-01-25 13:26:37 +0800

    添加书籍两个页面

---
+++ abhegios/Class/BookStore/BookSearchResultTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  BookSearchResultTableViewController.h
+//  abhegios
+//
+//  Created by Archer_LJ on 15/1/25.
+//  Copyright (c) 2015年 180. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BookSearchResultTableViewController : UITableViewController
+
+@end
