e591ba44de99d8c8b3d2452e1e59fb23e3f9f296
archer <archer@miglab.com>
2015-03-29 21:12:25 +0800

    添加并指向影视文件

---
+++ abhegios/Class/Movie/MovieCenterViewController.h
@@ -0,0 +1,14 @@
+//
+//  MovieCenterViewController.h
+//  abhegios
+//
+//  Created by Archer_LJ on 15/3/29.
+//  Copyright (c) 2015年 180. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "RefreshTableViewController.h"
+
+@interface MovieCenterViewController : RefreshTableViewController
+
+@end
---
+++ abhegios/Class/Movie/MovieCenterViewController.m
@@ -0,0 +1,44 @@
+//
+//  MovieCenterViewController.m
+//  abhegios
+//
+//  Created by Archer_LJ on 15/3/29.
+//  Copyright (c) 2015年 180. All rights reserved.
+//
+
+#import "MovieCenterViewController.h"
+
+@interface MovieCenterViewController ()
+
+@end
+
+@implementation MovieCenterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didRequest {
+    
+}
+
+#pragma mark - Table view data source
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
