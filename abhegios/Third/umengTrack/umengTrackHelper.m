c9b45892ee1eb5e68fbeaa5d09688b7fc0640b28
kerry <kerry@miglab.com>
2015-04-27 21:50:06 +0800

    +补充自动更新回调函数

--- abhegios/Third/umengTrack/umengTrackHelper.m
+++ abhegios/Third/umengTrack/umengTrackHelper.m
@@ -24,8 +24,29 @@ HELPER_SHARED
     //   channelId 为NSString * 类型，channelId 为nil或@""时,默认会被被当作@"App Store"渠道
     
     //[MobClick checkUpdate];   //自动更新检查, 如果需要自定义更新请使用下面的方法,需要接收一个(NSDictionary *)appInfo的参数
-    //[MobClick checkUpdateWithDelegate:self selector:@selector(updateMethod:)];
+    [MobClick checkUpdateWithDelegate:self selector:@selector(updateMethod:)];
     
     [MobClick updateOnlineConfig];  //在线参数配置
+}
+
+//自动更新
+-(void)updateMethod:(NSDictionary *)appInfo {
+    NSLog(@"update info %@",appInfo);
+    if([[appInfo objectForKey:@"update"] isEqualToString:@"YES"]==YES)
+    {
+        NSString *newVersion = [[NSString alloc]initWithString:[appInfo objectForKey:@"version"]];
+        self.path = [[NSString alloc]initWithString:[appInfo objectForKey:@"path"]];
+        UIAlertView *alert = [[UIAlertView alloc]initWithTitle:[NSString stringWithFormat:@"有新版本V%@",newVersion] message:[NSString stringWithString:[appInfo objectForKey:@"update_log"]] delegate:self cancelButtonTitle:@"下次再说" otherButtonTitles:@"更新", nil];
+        [alert show];
+        
+    }
+}
+
+-(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if(buttonIndex==1)
+    {
+        NSURL *url = [NSURL URLWithString: self.path];  [[UIApplication sharedApplication]openURL:url];
+    }
 }
 @end
